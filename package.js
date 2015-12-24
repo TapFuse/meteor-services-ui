16deb56abbaa84c47cc5c9988cdb3681d81af651
Matas <matasjonys@gmail.com>
2015-12-24 16:02:27 +0700

    Added client side collection

--- package.js
+++ package.js
@@ -19,23 +19,26 @@ Package.onUse(function(api) {
     // Core
     api.use([
       'templating',
-      'ecmascript'
+      'ecmascript',
+      'mongo',
     ]);
     // 3rd party
     api.use([
       'lauricio:less-autoprefixer@2.5.0_3',
       'mquandalle:jade@0.4.5',
+      'tapfuse:collection-global@1.0.0',
       // UI Base
       'ox2:font-roboto-condensed@1.13.0',
       'ox2:font-roboto@1.15.0',
       'ox2:normalize@3.0.2_1',
       'ox2:typography@1.2.0',
       'ox2:colors@1.4.0',
-      'ox2:buttons@1.3.0'
+      'ox2:buttons@1.3.0',
     ]);
     api.addFiles('lib/tp-services-ui.jade', C);
     api.addFiles('lib/tp-services-ui.less', C);
     api.addFiles('lib/tp-services-ui.js', C);
+    api.addFiles('lib/globals-client.js', C)
 });
 
 Package.onTest(function(api) {
